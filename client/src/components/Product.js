cc42526fc7fd0781f73f11e0951531e1b049ea80
pakut2 <kutny.pawel@gmail.com>
2021-06-15 01:02:05 +0200

    product cards

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
@@ -17,9 +18,10 @@ const Product = ({ product }) => {
         </a>
 
         <Card.Text as="div">
-          <div className="my-3">
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
