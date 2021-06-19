c293712ac5bbfb2697a81aca778cf6107ab16a9f
pakut2 <kutny.pawel@gmail.com>
2021-06-19 23:25:17 +0200

    register

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from "redux";
 import { productListReducer, productDetailsReducer } from "./productReducers";
 import { cartReducer } from "./cartReducers";
-import { userLoginReducer } from "./userReducers";
+import { userLoginReducer, userRegisterReducer } from "./userReducers";
 
 export default combineReducers({
   productListReducer,
   productDetailsReducer,
   cartReducer,
   userLoginReducer,
+  userRegisterReducer,
 });
--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -3,6 +3,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -17,6 +20,21 @@ export const userLoginReducer = (state = {}, action) => {
       return { loading: false, error: payload };
     case USER_LOGOUT:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (state = {}, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true };
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: payload };
+    case USER_REGISTER_FAIL:
+      return { loading: false, error: payload };
     default:
       return state;
   }
