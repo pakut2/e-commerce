aef264968d4b14ee7ccca04bf3986c1cf10fdeee
pakut2 <kutny.pawel@gmail.com>
2021-06-29 15:41:58 +0200

    create review

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -49,10 +49,10 @@ const createProduct = asyncHandler(async (req, res) => {
     user: req.user._id,
     image: "/images/sample.jpg",
     brand: "Sample brand",
-    category: "Sample category",
+    category: "Sample Category",
     countInStock: 0,
     numReviews: 0,
-    description: "Sample desc",
+    description: "Sample Description",
   });
 
   const createdProduct = await product.save();
@@ -85,10 +85,50 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @route   POST api/products/:id/review
+// @desc    Create new review
+// @access  private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() == req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product already reviewed");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res.status(201).json({ message: "Review added" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
 export {
   getProducts,
   getProductById,
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 };
