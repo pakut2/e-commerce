9b5959b5985001f457898c33b54b05152ad995ee
pakut2 <kutny.pawel@gmail.com>
2021-06-30 12:39:28 +0200

    pagination

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from "../models/Product.js";
 // @desc    GET all products
 // @access  public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 6;
+  const pageSize = 10;
   const page = Number(req.query.pageNumber) || 1;
 
   const keyword = req.query.keyword
